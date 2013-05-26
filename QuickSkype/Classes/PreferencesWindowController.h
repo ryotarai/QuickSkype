9a9d84a97745b847ff9460390454eaeff4e728c8
Ryota Arai <ryota.arai@gmail.com>
2013-05-27 08:33:56 +0900

    Add preferences window.

---
+++ QuickSkype/Classes/PreferencesWindowController.h
@@ -0,0 +1,13 @@
+//
+//  PreferencesWindowController.h
+//  QuickSkype
+//
+//  Created by Ryota Arai on 5/24/13.
+//  Copyright (c) 2013 Ryota Arai. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PreferencesWindowController : NSWindowController
+
+@end
